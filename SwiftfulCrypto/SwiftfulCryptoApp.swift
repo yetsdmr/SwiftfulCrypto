8a50fe5f75353cd07d9360776dd787b1faf8a0b0
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-24 22:43:29 +0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Yunus Emre Taşdemir on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
